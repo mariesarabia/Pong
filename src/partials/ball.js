0f454c3944d6b80a4401b5342a76eae84a9550ab
Marie <marie@Lawrences-MacBook-Air.local>
2017-08-04 10:12:11 -0700

    added ball movement and wall collision method

--- src/partials/ball.js
+++ src/partials/ball.js
@@ -6,14 +6,48 @@ export default class Ball {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     this.direction = 1;
+    this.reset();
+    this
   }
 
-  render(svg){
-      let circle = document.createElementNS(SVG_NS, 'circle');
-        circle.setAttributeNS(null, 'r', this.radius);
-        circle.setAttributeNS(null, 'fill', '#fff');
-        circle.setAttributeNS(null, 'cx', this.boardWidth / 2);
-        circle.setAttributeNS(null, 'cy', this.boardHeight /2);
-        svg.appendChild(circle);
+  reset() {
+    this.x = this.boardWidth / 2;
+    this.y = this.boardHeight / 2;
+
+    this.vy = 0;
+
+    while( this.vy === 0 ) {
+    this.vy = Math.floor(Math.random() * 10 - 5);
+    }
+
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
+  
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+    if (hitLeft || hitRight) {
+      this.vx = -this.vx
+    } else if (hitTop || hitBottom) {
+      this.vy = -this.vy
+    }
+  }
+
+  render(svg) {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.wallCollision();
+
+    let circle = document.createElementNS(SVG_NS, 'circle');
+    circle.setAttributeNS(null, 'r', this.radius);
+    circle.setAttributeNS(null, 'fill', '#fff');
+    circle.setAttributeNS(null, 'cx', this.x);
+    circle.setAttributeNS(null, 'cy', this.y);
+    svg.appendChild(circle);
+  }
+
 }
